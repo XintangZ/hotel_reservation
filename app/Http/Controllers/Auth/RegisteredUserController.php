af3e97bdc2a2852f9e5a5c5edb95fa8064c37102
Xintang ZHANG <xtzhang1994@gmail.com>
2024-04-29 13:50:19 -0400

    added phone validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,9 +33,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string', 'max:255', 'unique:'.User::class],    // TODO: add regrex validation
+            'phone' => ['required', 'string', 'size:14', 'regex:/^([0-9\s\-\+\(\)]*)$/', 'unique:'.User::class], 
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'phone.size' => 'The phone number is invalid.',
+            'phone.regex' => 'The phone number is invalid.',
+            'phone.unique' => 'The phone number has already been used by another user.',
+            'email.unique' => 'The email address has already been used by another user.',
         ]);
 
         $user = User::create([
--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,8 +18,18 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'first_name' => ['string', 'max:255'],
             'last_name' => ['string', 'max:255'],
-            'phone' => ['string', 'max:10', Rule::unique(User::class)->ignore($this->user()->id)],  //TODO: add validation
+            'phone' => ['string', 'size:14', 'regex:/^([0-9\s\-\+\(\)]*)$/', Rule::unique(User::class)->ignore($this->user()->id)],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'phone.size' => 'The phone number is invalid.',
+            'phone.regex' => 'The phone number is invalid.',
+            'phone.unique' => 'The phone number has already been used by another user.',
+            'email.unique' => 'The email address has already been used by another user.',
         ];
     }
 }
