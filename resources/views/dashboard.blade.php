f6ab09d4459fe6465a5aa31c61234d9915e8ecd7
Xintang ZHANG <xtzhang1994@gmail.com>
2024-04-28 21:32:45 -0400

    added delete confirmation modal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,13 +44,36 @@
                         <form action="{{ route('reservation.delete', $reservation->id) }}" method="post" class="flex justify-center">
                             @csrf
                             @method('DELETE')
-                            <x-secondary-button type="submit">{{ $isPast ? 'Delete' : 'Cancel'}} Reservation</x-secondary-button>
+                            <x-secondary-button data-modal-target="delete-modal" data-modal-toggle="delete-modal">{{ $isPast ? 'Delete' : 'Cancel'}} Reservation</x-secondary-button>
+
+                            <!-- delete confirmation modal -->
+                            <div id="delete-modal" data-modal-backdrop="static" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                <div class="relative p-4 w-full max-w-md max-h-full">
+                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="delete-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                        <div class="p-4 md:p-5 text-center">
+                                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                            </svg>
+                                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to {{ $isPast ? 'delete' : 'cancel'}} this reservation?</h3>
+                                            <x-danger-button type="submit" class="me-2">Yes, delete</x-danger-button>
+                                            <x-secondary-button data-modal-hide="delete-modal">No, go back</x-secondary-button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </form>
                         @if(!$isPast)
                         <div class="flex justify-center">
-                            <a href="{{ route('reservation.edit', $reservation->id) }}" class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white text-center uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                Change Reservation
-                            </a>
+                            <x-primary-button-a href="{{ route('reservation.edit', $reservation->id) }}">
+                                {{ __('Change Reservation' )}}
+                            </x-primary-button-a>
                         </div>
                         @endif
                     </div>
